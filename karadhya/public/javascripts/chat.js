b99b70e03a268dcd67c95478cc4513257b4f51ce
karthik m <karthikraj1996@gmail.com>
2017-09-16 07:53:05 +0000

    fifthy commit

--- karadhya/public/javascripts/chat.js
+++ karadhya/public/javascripts/chat.js
@@ -1,3 +1,25 @@
 var app=angular.module('chatApp',['ngMaterial']);
 app.controller('chatController',function($scope){
+	$scope.messages = [
+	{
+    	sender:"BOT",
+    	text: "What can i do for u?????",
+    	time:"1.42pm"
+	},
+	{
+    	sender:"user",
+    	text: "who is ashish",
+    	time:"2.42pm"
+	},
+	{
+    	sender:"BOT",
+    	text: "waste fellow",
+    	time:"2.42pm"
+	
+	}];
+	var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+	exampleSocket.onmessage  =   function  (event) {
+       var jsonData = JSON.parse(event.data);
+       console.log(jsonData);    
+   };
 });
